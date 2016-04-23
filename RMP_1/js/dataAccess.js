fca610aab45a5139be06024cbb4fd908b6b19224
lynprogram <772309141@qq.com>
2016-04-23 23:19:58 +0800

    test

--- RMP_1/js/dataAccess.js
+++ RMP_1/js/dataAccess.js
@@ -81,19 +81,20 @@ function isString(str)
  * @param goodCallBack  :have default function
  * @param errorCallBack :have default function
  */
-function post(tableNameWithId, dataStr, goodCallBack, errorCallBack)
+function post(tableNameWithId, dataStr, goodCallBack, errorCallBack,isAsync)
 {
     if (!isString(dataStr))
         dataStr = JSON.stringify(dataStr);
     //alert(dataStr);
     if (goodCallBack == undefined) goodCallBack = defaultSuccess;
     if (errorCallBack == undefined) errorCallBack = defaultError;
-
+    if (isAsync == undefined) isAsync = true;
 
     $.ajax({
         type: "POST",
         url: EntityUrl + tableNameWithId,
         dataType: "json",
+        async: isAsync,
         data: dataStr,
         contentType: "application/json; charset=utf-8",
         headers: {passwd: RMPPwd},
