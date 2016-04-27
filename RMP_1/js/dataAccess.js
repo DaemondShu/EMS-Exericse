57cdf330ae11f139d80e536be67af2b9f261d671
monkey_d_asce <405752182@qq.com>
2016-04-27 16:28:51 +0800

    finish rdf

--- RMP_1/js/dataAccess.js
+++ RMP_1/js/dataAccess.js
@@ -325,3 +325,37 @@ function createXML(obj)
     result += "</rdf:RDF>";
     return result;
 }
+
+
+function fake_click(obj) {
+    var ev = document.createEvent("MouseEvents");
+    ev.initMouseEvent(
+        "click", true, false, window, 0, 0, 0, 0, 0
+        , false, false, false, false, 0, null
+    );
+    obj.dispatchEvent(ev);
+}
+
+function export_raw(name, data) {
+    var urlObject = window.URL || window.webkitURL || window;
+
+    var export_blob = new Blob([data]);
+
+    var save_link = document.createElementNS("http://www.w3.org/1999/xhtml", "a")
+    save_link.href = urlObject.createObjectURL(export_blob);
+    save_link.download = name;
+    fake_click(save_link);
+}
+
+
+function downloadRdfXML()
+{
+    get("Paper","",function (json)
+    {
+        var data = getFirstAttr(json);
+        var xmlString = createXML(data);
+        //var temp = loadXML(xmlString);
+
+        export_raw("paperXml.xml" , xmlString);
+    })
+}
